39bcfbefe1ca9d3c5b53a9f39104f8aa7fc3c85b
valentinarosidelvalle-cloud <valentinarosidelvalle@gmail.com>
2026-03-18 20:12:55 +0000

    agrego y actualizo firebase config

--- firebase-config.js
+++ firebase-config.js
@@ -1,20 +1,16 @@
-// Firebase configuration file
-// Reemplaza estos valores con los datos de tu proyecto Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID",
+  apiKey: "AIzaSyCOB7ofLrXqReBwNPk1LVbr-w2nyI30mTs",
+  authDomain: "frichnfries.firebaseapp.com",
+  projectId: "frichnfries",
+  storageBucket: "frichnfries.firebasestorage.app",
+  messagingSenderId: "752431193967",
+  appId: "1:752431193967:web:882b9ad4e15d9be1ec529a"
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { app, auth, db };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
